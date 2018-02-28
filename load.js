00237ac04181030448953e65c7f1a1f517ebdd21
Peter Joseph <contact@peterajoseph.com>
2018-02-28 08:19:34 +0800

    Use XMLHttpRequest to load script into blob

--- load.js
+++ load.js
@@ -1,13 +1,17 @@
 let body = document.getElementsByTagName('BODY')[0];
 let script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = document.currentScript.getAttribute('bundle');
 
-script.onloadstart() = function() {
-	console.log("loading");
-}
+// Create new script request
+let xhr = new XMLHttpRequest(), method = "GET", url = document.currentScript.getAttribute('bundle');
+xhr.responseType = 'blob';
 
-script.onload() = function() {
-	console.log("loaded");
+// Load the script
+xhr.open(method, url, true);
+xhr.onload = function () {
+  if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+	script.src = URL.createObjectURL(xhr.response);
 	body.appendChild(script);
+  }
 };
+xhr.send();
