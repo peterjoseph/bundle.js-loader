0ca0cbf7ce14b23257e8c9976b6456da6864ea74
Peter Joseph <contact@peterajoseph.com>
2018-03-01 19:38:05 +0800

    Display loading image

--- load.js
+++ load.js
@@ -1,6 +1,13 @@
+// Setup body and script objects
 let body = document.getElementsByTagName('BODY')[0];
 let script = document.createElement('script');
 script.type = 'text/javascript';
+
+// Push loading spinner to screen
+body.innerHTML +=
+	'<div style="position:absolute; z-index:100; top:50%; left: 50%; transform: translate(-50%, -50%);">' + 
+		'<img src="' + document.currentScript.getAttribute('spinner') + '" alt="Loading">' + 
+	'</div>';
 
 // Create new script request
 let xhr = new XMLHttpRequest(), method = "GET", url = document.currentScript.getAttribute('bundle');
